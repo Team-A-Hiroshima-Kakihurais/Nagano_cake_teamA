322de83d1b6e47ea1b61c5704a94b39e5a1440d8
MilaisTaso <livreblanche819@gmail.com>
2023-01-17 11:26:54 +0000

    add controller cart_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,9 @@
 class Item < ApplicationRecord
-  # 画像が無い場合、デフォルト画像を設定する（個人作業では必要)
-
   # 各モデルとの関係
   has_one_attached :image
   
-  def default_image
-    if !self.image.attached?
-      self.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default.png')), filename: 'default.png', content_type: 'image/png')
-    end
-  end
-  
-  
-  # イメージ画像の大きさを変更する
-  def add_default_image(width, height)
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/default.jpeg')
-      image.attach(io: File.open(file_path), filename: 'default.jpeg', content_type: 'image/jpeg')
-    end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+  # 税込表示用メソッド
+  def get_total_payment(tax)
+    (self.price * tax.to_f).floor
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+ 
   scope module: :public do
     get 'deliveries/index'
     get 'deliveries/edit'
@@ -8,12 +13,10 @@ Rails.application.routes.draw do
     root to: "homes#top"
     get 'about' => 'homes#about', as: 'about'
     resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :create, :update, :destroy]
+    delete 'cart_items/delete_all', to: 'cart_items#destory_all', as: 'destory_all'
   end 
 
   
-  devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
