919c225bc218c39e4839f5e41785c0eb9b6498ac
EC2 Default User <ec2-user@ip-172-31-9-127.ap-northeast-1.compute.internal>
2023-01-17 03:02:03 +0000

    Add public/homecustomerdeliveries

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'deliveries/index'
+    get 'deliveries/edit'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :public do
+    root to: "homes#top"
+    get 'about' => 'homes#about', as: 'about'
+  end 
+
+  
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
