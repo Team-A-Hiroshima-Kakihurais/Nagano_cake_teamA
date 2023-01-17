d102dc22b044e14ed88ef8188d2423dd188144df
EC2 Default User <ec2-user@ip-172-31-9-127.ap-northeast-1.compute.internal>
2023-01-17 03:19:36 +0000

    model and contoroller are completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_17_023551) do
+ActiveRecord::Schema.define(version: 2023_01_17_031547) do
 
   create_table "customers", force: :cascade do |t|
     t.string "last_name"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2023_01_17_023551) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "deliveries", force: :cascade do |t|
+    t.string "post_code"
+    t.string "address"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
