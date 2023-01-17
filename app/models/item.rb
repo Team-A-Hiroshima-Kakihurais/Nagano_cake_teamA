754638e2bf16063700bde05e4213f55caf6042fb
MilaisTaso <livreblanche819@gmail.com>
2023-01-17 12:23:16 +0000

    add item model method

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
   # 各モデルとの関係
   has_one_attached :image
   
-  # 税込表示用メソッド
-  def get_total_payment(tax)
+  # 税込金額を求めるメソッド
+  def add_total_payment(tax)
     (self.price * tax.to_f).floor
   end
 end
