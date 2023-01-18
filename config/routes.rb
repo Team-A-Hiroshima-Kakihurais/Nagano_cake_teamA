9d29b828e13cb8539c9883293f8f7b46eff113dc
SM0322 <kaizer956@gmail.com>
2023-01-18 03:24:15 +0000

    :re

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  namespace :public do
-    get 'deliveries/index'
-    get 'deliveries/edit'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    root to: "homes#top"
-    get 'about' => 'homes#about', as: 'about'
-  end 
-
-  
-  devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-  devise_for :users
-=======
   
   devise_for :customers,skip:[:password],controllers:{
     registrations: "public/registrations",
@@ -29,6 +7,5 @@ Rails.application.routes.draw do
   devise_for :admins,skip:[:registrations, :password], controllers:{
     sessions: "admin/sessions"
   }
->>>>>>> origin/develop
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
