883bf19a89880827b75404a8dd457d09eb17f9d1
RNrd00 <117254446+RNrd00@users.noreply.github.com>
2023-01-19 05:23:13 +0000

    Update edit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -12,11 +12,13 @@ class Admin::ItemsController < ApplicationController
     end
     
     def edit
+        @item = Item.find(params[:id])
     end
     
     def update
+        @item = Item.find(params[:id])
         @item.update(admin_params)
-            redirect_to admin_item_path(@item)
+        redirect_to admin_item_path(@item)
     end
     
     def create
