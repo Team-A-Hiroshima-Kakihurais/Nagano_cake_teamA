ceb24706cb9132e895547428d1beb21559accee6
SM0322 <kaizer956@gmail.com>
2023-01-23 04:59:48 +0000

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -63,18 +63,18 @@ ActiveRecord::Schema.define(version: 2023_01_23_003450) do
   end
 
   create_table "customers", force: :cascade do |t|
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.string "post_code", null: false
-    t.string "address", null: false
-    t.integer "phone_number", null: false
-    t.boolean "is_deleted", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "post_code"
+    t.string "address"
+    t.string "phone_number"
+    t.boolean "is_deleted", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
