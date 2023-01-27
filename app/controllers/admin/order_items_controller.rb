0fe2fed982a85ee2071ec7359091023102b10168
RNrd00 <117254446+RNrd00@users.noreply.github.com>
2023-01-27 01:15:50 +0000

    Fix

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -3,7 +3,19 @@ class Admin::OrderItemsController < ApplicationController
     
     def update
         @order_item = OrderItem.find_by(id: params[:id])
-        @order_item.update(orderitem_params)
+        @order = @order_item.order
+        @order_items = @order.order_items.all
+        
+        is_updated = true
+    if  @order_item.update(orderitem_params)
+        @order.update(status: 2) if @order_item.making_status == "in_production" 
+        @order_items.each do |order_item| #　紐付いている注文商品の製作ステータスを一つ一つeach文で確認していきます。
+     if order_item.making_status != "completed" # 製作ステータスが「製作完了」ではない場合 
+        is_updated = false # 上記で定義してあるis_updatedを「false」に変更する。
+     end
+    end
+    @order.update(status: 3) if is_updated
+    end
         redirect_back(fallback_location: admin_order_path)
     end
     
